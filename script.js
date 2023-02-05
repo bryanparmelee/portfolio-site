c75674a623219a316871891d2cdde693f4787998
bryanparmelee <bryan.parmelee@gmail.com>
2023-02-05 13:24:34 -0500

    Add section transistions on scroll

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ const mobileNav = document.querySelector(".mobile-nav");
 const navLink = document.querySelectorAll(".nav-link");
 const navbar = document.querySelector(".navbar");
 const projectsContainer = document.querySelector(".projects-container");
+
 const year = document.querySelector(".year");
+
+
+
+
 
 burger.addEventListener("click", () => {
   mobileNav.classList.toggle("hide");
@@ -48,5 +53,37 @@ const showProjects = () => {
 }
 
 showProjects();
+
+const projectCards = document.querySelectorAll(".project-card");
+const skillsContainer = document.querySelector(".skills-container");
+
+function checkLocation() {
+  let height = window.innerHeight;
+  let skillsRect = skillsContainer.getBoundingClientRect();
+  let skillsHeight = skillsRect.y;
+  if(height / 2 > skillsHeight) {
+    skillsContainer.classList.add("show-skills");
+  }
+  if (0 - height > skillsHeight || height < skillsHeight) {
+    skillsContainer.classList.remove("show-skills");
+  }
+  
+  projectCards.forEach((card, i) => {
+    let rect = card.getBoundingClientRect();
+    let cardHeight = rect.y;
+    if (height / 2 > cardHeight) {      
+      card.classList.add("show-proj");
+    } 
+    if (0 - height > cardHeight || height < cardHeight) {
+      card.classList.remove("show-proj");
+    }
+  });
+   
+
+}
+
+checkLocation();
+
+window.addEventListener('scroll', () => checkLocation());
 
 year.textContent = new Date().getFullYear();
