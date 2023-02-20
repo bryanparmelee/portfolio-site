fca11651a5fa8b713eb51ca89a85d667b69b8b62
bryanparmelee <bryan.parmelee@gmail.com>
2023-02-20 18:01:55 -0500

    Add project, resume updates

--- project-data.js
+++ project-data.js
@@ -1,40 +1,54 @@
 const PROJECT_DATA = [
-    {
-        id: 'netflix-clone',
-        title: 'Netflix Clone',
-        image: './Assets/Images/testflix.jpg',
-        liveUrl: 'https://netflix-clone-bp.netlify.app/',
-        codeUrl: 'https://github.com/bryanparmelee/netflix-clone',
-        tech: 'React, Tailwind CSS, The Movie Database API',
-        description: 'A failthful clone of the Netflix home page including their category slider with progress indicator and hover effect. Uses the Movie Database API to ensure the featured movies are updated on each visit.',
-    },
-    {
-        id: 'deal-loft',
-        title: 'Deal Loft',
-        image: './Assets/Images/deal-loft.jpg',
-        liveUrl: 'https://dealloft.netlify.app',
-        codeUrl: 'https://github.com/bryanparmelee/ecommerce-site',
-        tech: 'React, React Router, Tailwind CSS, Firebase, Stripe',
-        description: 'Deal loft is an e-commerce site that utilizes Google Firebase for authentication and storage and the Stripe API for payment processing.'
-    },
-    {
-        id: 'goals',
-        title: 'Goals You May Have Missed',
-        image: './Assets/Images/goals.jpg',
-        liveUrl: 'https://goalsyoumayhavemissed.netlify.app/',
-        codeUrl: 'https://github.com/bryanparmelee/goals-i-might-have-missed',
-        tech: 'React, Sass, Score Bat API',
-        description: 'So many soccer games, so little time. This site uses the ScoreBat API to gather highlight videos from recent matches in the biggest leagues.',
-    },
-    {
-        id: 'wedding',
-        title: 'Wedding Website',
-        image: './Assets/Images/wedding.jpg',
-        liveUrl: 'https://parmecitto.com/',
-        codeUrl: 'https://github.com/bryanparmelee/wedding-website',
-        tech: 'HTML5, CSS, Javascript',
-        description: 'I built this website for guests of my wedding in the summer of 2022. Features a wedding countdown (now, a countup) and responsive photo gallery.',
-    }
+  {
+    id: "deal-loft",
+    title: "Deal Loft",
+    image: "./Assets/Images/deal-loft.jpg",
+    liveUrl: "https://dealloft.netlify.app",
+    codeUrl: "https://github.com/bryanparmelee/ecommerce-site",
+    tech: "React, React Router, Tailwind CSS, Firebase, Stripe",
+    description:
+      "Deal loft is an e-commerce site that utilizes Google Firebase for authentication and storage and the Stripe API for payment processing.",
+  },
+  {
+    id: "netflix-clone",
+    title: "Netflix Clone",
+    image: "./Assets/Images/testflix.jpg",
+    liveUrl: "https://netflix-clone-bp.netlify.app/",
+    codeUrl: "https://github.com/bryanparmelee/netflix-clone",
+    tech: "React, Tailwind CSS, The Movie Database API",
+    description:
+      "A failthful clone of the Netflix home page including their category slider with progress indicator and hover effect. Uses the Movie Database API to ensure the featured movies are updated on each visit.",
+  },
+  {
+    id: "goals",
+    title: "Goals You May Have Missed",
+    image: "./Assets/Images/goals.jpg",
+    liveUrl: "https://goalsyoumayhavemissed.netlify.app/",
+    codeUrl: "https://github.com/bryanparmelee/goals-i-might-have-missed",
+    tech: "React, Sass, Score Bat API",
+    description:
+      "So many soccer games, so little time. This site uses the ScoreBat API to gather highlight videos from recent matches in the biggest leagues.",
+  },
+  {
+    id: "context",
+    title: "Out of Context",
+    image: "./Assets/Images/out-of-context.jpg",
+    liveUrl: "https://outofcontext.space/",
+    codeUrl: "https://github.com/bryanparmelee/out-of-context",
+    tech: "React, Tailwind",
+    description:
+      "Out of Context is a random quote generator that justaposes humerous witticisms from a client's Slack channel with inspiring galactic backgrounds.",
+  },
+  {
+    id: "wedding",
+    title: "Wedding Website",
+    image: "./Assets/Images/wedding.jpg",
+    liveUrl: "https://parmecitto.com/",
+    codeUrl: "https://github.com/bryanparmelee/wedding-website",
+    tech: "HTML5, CSS, Javascript",
+    description:
+      "I built this website for guests of my wedding in the summer of 2022. Features a wedding countdown (now, a countup) and responsive photo gallery.",
+  },
 ];
 
 export default PROJECT_DATA;
